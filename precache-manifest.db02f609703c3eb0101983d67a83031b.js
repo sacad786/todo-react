a8c629c79608d24e6bb1dc4ae7b36c006b449f69
sacad786 <sacad786@gmail.com>
2020-06-25 16:40:45 +0200

    Updates

---
+++ precache-manifest.db02f609703c3eb0101983d67a83031b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81a7c49225f7ed2ec065b25c1bee8c8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "975ff9eb5fb98a4597a9",
+    "url": "/monster-rolodex/static/css/main.fd10fc7e.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/monster-rolodex/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975ff9eb5fb98a4597a9",
+    "url": "/monster-rolodex/static/js/main.f049d534.chunk.js"
+  },
+  {
+    "revision": "8299370f6f3fcee7b4fa",
+    "url": "/monster-rolodex/static/js/runtime-main.6608e69f.js"
+  }
+]);
