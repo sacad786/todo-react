e6da564af8d0820df706116f0c6f99cbe0f7d67f
sacad786 <sacad786@gmail.com>
2020-07-05 11:35:59 +0200

    tests working up to display tasks

--- src/Components/DisplayTasks/index.test.js
+++ src/Components/DisplayTasks/index.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import { createMount } from "@material-ui/core/test-utils";
 import DisplayTasks from "./index";
+
 const task = { name: "", description: "" };
 
 it("renders DisplayTasks without crashing", () => {
@@ -29,6 +30,28 @@ it("when there's tasks it should display the tasks", () => {
   );
   expect(wrapper.find("#tasks")).toHaveLength(1);
 });
+
+
+/* let wrapper;
+
+      beforeEach(() => {
+          wrapper = shallow( <ChannelList { ...props}/>).dive();
+      });
+
+        test("handleClick is called when clicking on a list item", () => {
+          const spy = sinon.spy(wrapper.instance(), "handleClick");
+          const button1 = wrapper.find({
+            "data-testid": "test-channel10"
+          }).dive();
+          expect(button1).toHaveLength(1);
+          console.log(button1.debug());
+          button1.simulate("click");
+          expect(spy.called).toBe(true);
+        });
+});
+
+*/
+
 
 // it("should call handleUpdateTasks when submit button is clicked", () => {
 //     const wrapper = shallow(
--- src/Components/LoginPage/index.test.js
+++ src/Components/LoginPage/index.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { createMount } from '@material-ui/core/test-utils';
 import LoginPage from './index';
+import sinon from "sinon";
 
 
 
@@ -31,4 +32,19 @@ it('should call handleLoginChange when submit button is clicked', () => {
 })
 
 
+// test('should ', () => {
+//      const  wrapper = shallow(<LoginPage getUserRequested = {() => {}} />);
+
+//       const spy = sinon.spy(wrapper.instance(), "handleLoginChange");
+//       const button1 = wrapper.find('.login-btn')
+//   expect(button1.exists()).toBe(true)
+
+//       expect(button1).toHaveLength(1);
+//       console.log(button1.debug());
+//       button1.simulate("click");
+//       expect(spy).toHaveOwnProperty('callCount', 1);
+//     })
+
 // shallow rending vs mount vs createMount vs
+
+// how to check if funciion on button is called - sinon spy
